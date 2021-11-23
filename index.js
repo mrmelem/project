aaa1c1ac22c6034188a69a7af1db3d60e58ebf44
Pedro Melém <melem.dev@gmail.com>
2021-11-23 11:39:52 -0300

    1.2.0

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require(express)
 const app = express()
 app.use(require(cors)())
 const port = process.env.PORT || 3000
+
+app.get('/', (req,res)=>{
+    return res.send("Olá mundo")
+})
+
 app.listen(pot, ()=>{
     console.log("Servidor online")
 })
